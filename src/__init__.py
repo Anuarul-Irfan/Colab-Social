d7324d52920e439e1f6f14f567c91ea8bfce5c01
Anuarul Irfan <75465459+Anuarul-Irfan@users.noreply.github.com>
2024-01-13 21:59:24 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 app=Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///C:\\Users\\USER\\Downloads\\Forum.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///C:\\Users\\USER\\Downloads\\Forum1.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.config["SECRET_KEY"]=os.urandom(32)
 app.config["DEBUG"]=True
