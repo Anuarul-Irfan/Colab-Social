35a504dcafcd4910d2cedaf8b94e2574d0382124
Anuarul Irfan <75465459+Anuarul-Irfan@users.noreply.github.com>
2024-01-14 16:12:22 +0800

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,8 +1,8 @@
 import enum
 from src import db
 from datetime import datetime
-from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
 from flask_login import UserMixin
+import requests
 
 """
     Blueprint of the database schema   
@@ -54,8 +54,11 @@ class User(UserMixin,db.Model):
             }
             
 
-class OAuth(OAuthConsumerMixin, db.Model):
+class OAuth(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    provider = db.Column(db.String(50), nullable=False)
     provider_user_id = db.Column(db.String(256), unique=True)
+    token = db.Column(db.JSON)
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     user = db.relationship(User)
 
@@ -95,4 +98,3 @@ class Comment(db.Model):
             "time": time_difference(self.created_at),
             "upvotes": self.likes
         }
-
