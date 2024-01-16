8134f77d00b186168211c7e574200c48d658ffcf
Anuarul Irfan <75465459+Anuarul-Irfan@users.noreply.github.com>
2024-01-17 00:43:19 +0800

    Update routes.py

--- src/routes.py
+++ src/routes.py
@@ -66,7 +66,7 @@ def login():
     condition= current_user.is_authenticated and google.authorized
     if not condition:
         return redirect(url_for('google.login'))
-    return redirect(url_for('index'))
+    return redirect(url_for(request.referrer or url_for('index'))
 
 @oauth_authorized.connect_via(google_blueprint)
 def google_logged_in(blueprint, token):
